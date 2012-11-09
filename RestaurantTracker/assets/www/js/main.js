ef26286f1b4ab310104eb75f5188377dd6470f62
Cora Charneskey <charneskey@gmail.com>
2012-11-09 11:58:56 -0700

    added some parse code but not working yet.

--- RestaurantTracker/assets/www/js/main.js
+++ RestaurantTracker/assets/www/js/main.js
@@ -1,6 +1,6 @@
-//TODO: insert parse keys
-Parse.initialize("", "");
-//TipObject = Parse.Object.extend("TipObject");
+
+Parse.initialize("Gs9WRdTYTb4cTZdUA1PzBKj3aKy4Yw4CSFDAAqoy", "jnk4wFOf2UdekynXp1P0msNWDrtEkWo3IZ7vfF9x");
+RObject = Parse.Object.extend("RObject");
 
 function onDeviceReady(){
 	alert("device ready");
@@ -10,3 +10,18 @@ function onDeviceReady(){
 function init(){
 	document.addEventListener("deviceready", onDeviceReady, true);
 }
+
+$("#trackingForm").on("submit",function(e){
+	
+	e.preventDefault();
+	var name = $("#rName").val();
+	var meal = $("#rMeal").val();
+	var city = $("#rCity").val();
+	
+	var r = new RObject();
+	
+	r.save({name:name});
+}
+		
+
+);
