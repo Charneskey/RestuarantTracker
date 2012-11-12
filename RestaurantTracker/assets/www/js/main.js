79ff4d9a6b197266b01170fe5cb986b16b6b7a2e
Cora Charneskey <charneskey@gmail.com>
2012-11-11 20:38:07 -0700

    submit button is working and data is being logged to parse

--- RestaurantTracker/assets/www/js/main.js
+++ RestaurantTracker/assets/www/js/main.js
@@ -1,27 +1,27 @@
 
 Parse.initialize("Gs9WRdTYTb4cTZdUA1PzBKj3aKy4Yw4CSFDAAqoy", "jnk4wFOf2UdekynXp1P0msNWDrtEkWo3IZ7vfF9x");
-RObject = Parse.Object.extend("RObject");
+var RObject = Parse.Object.extend("RObject");
 
 function onDeviceReady(){
 	alert("device ready");
+	var r = new RObject();
 	
+	$("#trackingForm").on("submit",function(e){
+			
+			e.preventDefault();
+			var name = $("#rName").val();
+			var meal = $("#rMeal").val();
+			var city = $("#rCity").val();
+					
+			r.save({name:name});
+			r.save({meal:meal});
+			r.save({city:city});
+			
+		});
+		
 }
 
 function init(){
 	document.addEventListener("deviceready", onDeviceReady, true);
 }
 
-$("#trackingForm").on("submit",function(e){
-	
-	e.preventDefault();
-	var name = $("#rName").val();
-	var meal = $("#rMeal").val();
-	var city = $("#rCity").val();
-	
-	var r = new RObject();
-	
-	r.save({name:name});
-}
-		
-
-);
